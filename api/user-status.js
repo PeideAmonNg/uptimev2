18f43b17c4a68713fec652878a55acfa481ddb49
PeideAmonNg <peide.amon.ng@gmail.com>
2021-02-10 08:08:52 +1300

    Change user status list logic

--- api/user-status.js
+++ api/user-status.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 
 const password = process.env.MONGODB_ADMIN_PASS;
 
 mongoose.connect(`mongodb+srv://admin:${password}@cluster0.krz1f.mongodb.net/uptime?retryWrites=true&w=majority`, 
                   {useNewUrlParser: true, useUnifiedTopology: true});
 
+const db = mongoose.connection;
+autoIncrement.initialize(db);
+
+const User = require('../db/UserModel');
 const UserStatus = require('../db/UserStatusModel');
 
 function getUserStatuses(user) {
-  return UserStatus.find({status: 'online'}).sort({createdAt: -1}).limit(50).lean();
+  return User.count().then(count => {
+    console.log(count)
+    return UserStatus.find().sort({createdAt: -1}).limit(count).lean();
+  });  
 }
 
 module.exports = async (req, res) => {

4cabb2e8a7d0e5a7c05d2f6af975628dfe82238f
PeideAmonNg <peide.amon.ng@gmail.com>
2021-02-12 20:17:39 +1300

    Change isOnline logic and format dates

--- src/UserStatus.svelte
+++ src/UserStatus.svelte
@@ -15,15 +15,13 @@
       }
       isFetchingUserStatuses = false;
     } catch(e) {
-      console.log('caught error', e)
+      console.log('Caught error', e)
     }
-
-    
   });
 
   function formatDate(date) {
     let d = new Date(date);
-    return `${d.getHours()}:${d.getMinutes()}`;
+    return `${('0' + d.getHours()).slice(-2)}:${('0' + d.getMinutes()).slice(-2)}`;
   }
 
   let previousDayMonth = '-1/-1'; // initialise to bogus value
@@ -37,8 +35,18 @@
 
   function getDayMonth(date) {
     let d = new Date(date);
-    let dayMonth = `${d.getDate()}/${d.getMonth() + 1}`;
+    let dayMonth = `${('0' + d.getDate()).slice(-2)}/${('0' + d.getMonth() + 1).slice(-2)}`;
     return dayMonth;  
+  }
+
+  function isOnline(lastOnlineDate) {
+    let cronJobInterval = 5; // 5 minute interval
+    let delay = 2; // delay from when crob job is actually called plus processing time
+    if(Math.abs(new Date(lastOnlineDate).getMinutes() - new Date().getMinutes()) <= (cronJobInterval + delay))  {
+      return true;
+    } else {
+      return false;
+    }
   }
 </script>
 <main>
@@ -62,7 +70,10 @@
       <tr><td class="daymonth">{getDayMonth(us.createdAt)}</td></tr>
     {/if}
     <tr>
-      <td class="time">{formatDate(us.createdAt)}</td><td><a href="/users/{us.userid}">{us.username}</a></td><td><span class="{us.status}">{us.status}</span></td>
+      <td class="time">{formatDate(us.createdAt)}</td><td><a href="/users/{us.userid}">{us.username}</a></td>
+      {#if isOnline(us.createdAt)}
+        <td><span class="online">online</span></td>
+      {/if}
     </tr>
   {/each}
   </table>

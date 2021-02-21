d6d0772c501acb56de78e6e11d533b01af4e6add
PeideAmonNg <peide.amon.ng@gmail.com>
2021-02-21 13:51:19 +1300

    Change user online status visualisation on user page

--- api/user-status.js
+++ api/user-status.js
@@ -17,11 +17,105 @@ function getUserStatusById(userid, limit = 50) {
     if(!user) {
       return Promise.resolve([]);
     }
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() -1);
+    yesterday.setHours(0,0,0,0);
+
     return Promise.all([
-      user.name,
-      UserStatus.find({username: user.username}, {username: 1, createdAt: 1, status: 1}).sort({createdAt: -1}).limit(limit).lean()
-    ]);
+      user,
+      UserStatus.find({username: user.username, createdAt: {$gte: yesterday}}, {createdAt: 1, status: 1, _id: 0}).sort({createdAt: -1}).lean()
+    ]).then(values => {
+
+      let statuses = values[1];
+      
+      let days = getDays(statuses);
+
+      return [user, days];
+      
+    });
   });
+}
+
+function getHourMinute(date) {
+  let d = new Date(date);
+  return `${('0' + d.getHours()).slice(-2)}:${('0' + d.getMinutes()).slice(-2)}`;
+}
+
+function getDayMonthYear(ms) {
+  return getDayMonth(ms) + '/' + (new Date(ms).getFullYear());
+}
+
+function getDayMonth(date) {
+  let d = new Date(date);
+  let dayMonth = `${d.getDate()}/${d.getMonth() + 1}`;
+  return dayMonth;  
+}
+
+function getDays(statuses) {
+
+  // Model:
+  // let res = {
+  // 	days : {
+  // 		'11/02/2021': {
+  // 			hours: {
+  // 				'08:00': {
+  // 					status: {
+  // 						online: 5, 
+  // 						offline: 7
+  // 					}
+  // 				},
+  // 				'07:00': {
+  // 					status: {
+  // 						online: 0,
+  // 						offline: 12
+  // 					}
+  // 				},
+  // 				...
+  // 			}
+  
+  let days = {};
+  
+  // Get all statuses the past day
+  let now = new Date();    
+  let yesterday = new Date(now);
+  yesterday.setDate(yesterday.getDate() - 1);
+  yesterday.setHours(0,0,0,0);
+
+  let pastDay = new Date();
+  for (let i = 0; i < 24; i++) {
+
+    pastDay.setDate(pastDay.getDate() - 1);
+  }
+  
+  for (let s of statuses) {
+
+    let date = new Date(s.createdAt);
+    let dayMonthYear = getDayMonthYear(s.createdAt);
+
+    if (!(dayMonthYear in days)) {
+      days[dayMonthYear] = {};
+    }
+
+    let day = days[dayMonthYear];
+
+    if (!('hours' in day)) {
+      day['hours'] = {};
+    }
+
+    date.setMinutes(0);
+    let hourMinute = getHourMinute(date);
+
+    if (!(hourMinute in day.hours)) {
+      day.hours[hourMinute] = {
+        online: 0,
+        offline: 0
+      };
+    }
+
+    day.hours[hourMinute][s.status] = (day.hours[hourMinute][s.status] || 0) + 1
+  }
+
+  return days;
 }
 
 module.exports = async (req, res) => {
@@ -30,8 +124,14 @@ module.exports = async (req, res) => {
   }
   let userStatuses = [];
   if(req.query.id) {
-    [name, userStatuses] = await getUserStatusById(req.query.id);    
+    [user, userStatuses] = await getUserStatusById(req.query.id);    
+    console.log(userStatuses.length);
+    res.status(200).send({
+      name: user.name,
+      username: user.username,
+      userStatuses
+    });
+  } else {
+    res.status(200).send({userStatuses});
   }
-  console.log(userStatuses.length);
-  res.status(200).send({name, userStatuses});
 };
--- src/User.svelte
+++ src/User.svelte
@@ -3,23 +3,32 @@
   import Auth from './Auth';
   export let id;
   let name = '';
+  let username = '';
   let userStatuses = [];
   let isFetchingUser = false;
+  let days = {};
 
   onMount(async () => {    
     console.log('fetching apikey')
     let apiKey = Auth.getApiKey();
     console.log('apikey', apiKey)
     isFetchingUser = true;
-    const res = await fetch(`/api/user-status/${id}?api_key=${apiKey}`);
-    let o = await res.json();
-    userStatuses = o.userStatuses;
-    name = o.name;
+    try {
+      const res = await fetch(`/api/user-status/${id}?api_key=${apiKey}`);
+      let o = await res.json();
+      userStatuses = o.userStatuses;
+      name = o.name;
+      username = o.username;
+      days = o.userStatuses;
+      console.log(o)
+    } catch (e) {
+      console.log('Failed to fetch user status', e);
+    }
     isFetchingUser = false;
-    console.log(userStatuses);
   });
 
-  function formatDate(date) {
+
+  function getHourMinute(date) {
     let d = new Date(date);
     return `${('0' + d.getHours()).slice(-2)}:${('0' + d.getMinutes()).slice(-2)}`;
   }
@@ -38,6 +47,21 @@
     let dayMonth = `${d.getDate()}/${d.getMonth() + 1}`;
     return dayMonth;  
   }
+
+  function getStatusHsl(hour) {
+    let on = hour.online;
+    let off = hour.offline;
+    let onelineRate = Math.round((on / (on + off)) * 100);
+    // let lightness = 0.3 * prob + 30
+    
+    let lightness = -0.9 * onelineRate + 100;
+    let hsl = 'lightgray';
+    if (onelineRate > 0 ) {
+      hsl = `hsl(115, 100%, ${lightness}%)`;
+    }
+
+    return hsl;
+  }
 </script>
 
 <main>
@@ -46,18 +70,32 @@
       <img src="/dual-ring.gif" alt="Loading.." width="20" height="20"><span>Fetching..</span>
     </div>
   {/if}
-  <b>{userStatuses.length > 0 ? userStatuses[0].username : ''}</b>
+  <b>
+    {#if username }
+      <p>{username}</p>
+    {/if}
+  </b>
   {#if name }
     <p>{name}</p>
   {/if}
   <table>
-  {#each userStatuses as us}
-    {#if !sameDayMonthAsPrevious(us.createdAt)}
-      <tr><td class="daymonth">{getDayMonth(us.createdAt)}</td></tr>
-    {/if}
-    <tr>
-      <td class="time">{formatDate(us.createdAt)}</td>
-      <td class="{us.status}">{us.status}</td>
+  {#each Object.keys(days) as day}
+    <tr><td>{day.split('/').slice(0, 2).join('/')}</td>
+      {#each Object.keys(days[day].hours).sort() as hour}
+        <td>
+          <span 
+            class="tooltip"
+            style="width: 1em; height: 1em; vertical-align: middle; background-color: {getStatusHsl(days[day]['hours'][hour])}"
+            title={Math.round(days[day]['hours'][hour]['online'] / (days[day]['hours'][hour]['online'] + days[day]['hours'][hour]['offline']) * 100) + '% online rate'}
+          >
+            <span class="tooltiptext">
+              {hour}
+              <br>
+              {Math.round(days[day]['hours'][hour]['online'] / (days[day]['hours'][hour]['online'] + days[day]['hours'][hour]['offline']) * 100) + '% online'}
+            </span>
+        </span>
+        </td>
+      {/each}
     </tr>
   {/each}
 </table>
